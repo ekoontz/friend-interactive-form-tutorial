33995a0829864951d6b4c0d8a1bc194f0ea57cf3
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-17 12:53:25 -0700

    move stuff around

--- src/friend_form_login/handler.clj
+++ src/friend_form_login/handler.clj
@@ -4,6 +4,7 @@
             [compojure.route :as route]
             [clojure.data.json :as json]
             [friend-oauth2.workflow :as oauth2]
+            [clojure.tools.logging :as log]
             [friend-oauth2.util :refer [format-config-uri]]
             [environ.core :refer [env]]
             [org.httpkit.client :as http]
@@ -17,12 +18,6 @@
             "dave" {:username "dave"
                     :password (creds/hash-bcrypt "password")
                     :roles #{::user}}})
-
-(def client-config
-  {:client-id (env :google-client-id)
-   :client-secret (env :google-client-secret)
-   :callback {:domain (env :google-callback-domain)
-              :path "/oauth2callback"}})
 
 (derive ::admin ::user)
 
@@ -38,63 +33,56 @@
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (route/not-found "Not Found"))
 
-(defn token2username [access-token & [request]]
-  "Get user's email address from the vc_user table, given their access token. 
-   If access-token is not registered in database,
-   then ask Google for the user's email address and add as new row to the vc_user table.
-   _request_ is optional: if present, it allows for more diagnostic logging than without it.
-"
+(defn token2username [access-token]
+  "Get user's email address given their access token."
   (cond
    (nil? access-token)
-   (do
-     (throw (Exception. (str "token2user: supplied access-token was null."))))
+   (throw (Exception. (str "token2user: supplied access-token was null.")))
 
    true
    (do
+     (log/info (str "looking up username for google access-token: " access-token))
      (let [{:keys [status headers body error] :as resp} 
            @(http/get 
              (str "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" access-token))]
        (cond 
-                 
-             (not (= status 200))
-             (do
-               nil)
-                 
-             true
-             (do
-               (let [body (json/read-str body
-                                         :key-fn keyword
-                                         :value-fn (fn [k v]
-                                                     v))
-                     email (get body :email)
-                     given-name (get body :given_name)
-                     family-name (get body :family_name)
-                     picture (get body :picture)]
-                 email)))))))
+       
+         (not (= status 200))
+         (do
+           nil)
+       
+         true
+         (do
+           (let [body (json/read-str body
+                                     :key-fn keyword
+                                     :value-fn (fn [k v]
+                                                 v))
+                 email (get body :email)
+                 given-name (get body :given_name)
+                 family-name (get body :family_name)
+                 picture (get body :picture)]
+             email)))))))
 
-(defn credential-fn [token]
-  ;;lookup token in DB or whatever to fetch appropriate :roles
-;  (log/info (str "google/credential-fn token: " (:access-token token)))
-  (let [username (token2username (:access-token token))]
-    {:identity token :roles #{::user}}))
-  
-(def uri-config
-  {:authentication-uri {:url "https://accounts.google.com/o/oauth2/auth"
-                        :query {:client_id (:client-id client-config)
-                                :response_type "code"
-                                :redirect_uri (format-config-uri client-config)
-                                :scope "email"}}
-
-   :access-token-uri {:url "https://accounts.google.com/o/oauth2/token"
-                      :query {:client_id (:client-id client-config)
-                              :client_secret (:client-secret client-config)
-                              :grant_type "authorization_code"
-                              :redirect_uri (format-config-uri client-config)}}})
+(def client-config
+  {:client-id (env :google-client-id)
+   :client-secret (env :google-client-secret)
+   :callback {:domain (env :google-callback-domain)
+              :path "/oauth2callback"}})
 
 (def google-auth-config {:client-config client-config
-                         :uri-config uri-config
-                         :credential-fn credential-fn})
-
+                         :uri-config {:authentication-uri {:url "https://accounts.google.com/o/oauth2/auth"
+                                                           :query {:client_id (:client-id client-config)
+                                                                   :response_type "code"
+                                                                   :redirect_uri (format-config-uri client-config)
+                                                                   :scope "email"}}
+                                      :access-token-uri {:url "https://accounts.google.com/o/oauth2/token"
+                                                         :query {:client_id (:client-id client-config)
+                                                                 :client_secret (:client-secret client-config)
+                                                                 :grant_type "authorization_code"
+                                                                 :redirect_uri (format-config-uri client-config)}}}
+                         :credential-fn (fn [token]
+                                          (let [username (token2username (:access-token token))]
+                                            {:identity username :roles #{::user}}))})
 (def app
   (handler/site
    (friend/authenticate app-routes
