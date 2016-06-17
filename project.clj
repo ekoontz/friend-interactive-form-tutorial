18562f146b7cccf7ed6c62b16e4ac5ba42084e67
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-17 12:54:23 -0700

    upgrade dependencies to avoid puzzling 'java.lang.ClassNotFoundException: org.apache.http.util.Args' error when trying to 'lein ring server-headless'

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject friend-form-login "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.1.3"]
+                 [com.cemerick/friend "0.2.3"]
                  [environ "1.0.0"]
-                 [clj-http "0.7.3"]
+                 [clj-http "2.2.0"]
                  [http-kit "2.1.16"]
-                 [friend-oauth2 "0.1.3"]
+                 [friend-oauth2 "0.1.3"];; :exclusions [com.fasterxml.jackson.dataformat/jackson-dataformat-smile ring/ring-core clj-http]]
                  [org.clojure/data.json "0.2.5"] 
-                 [ring/ring-core "1.2.0"]
-                 [com.cemerick/friend "0.2.3"]]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [ring/ring-core "1.3.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler friend-form-login.handler/app}
-  :profiles
-  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+
