dc532e82ab6b24cbd8455e1172c21400a9c330cf
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-17 12:19:42 -0700

    add google auth support

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [compojure "1.1.3"]
+                 [environ "1.0.0"]
+                 [clj-http "0.7.3"]
+                 [http-kit "2.1.16"]
+                 [friend-oauth2 "0.1.3"]
+                 [org.clojure/data.json "0.2.5"] 
                  [ring/ring-core "1.2.0"]
                  [com.cemerick/friend "0.2.3"]]
   :plugins [[lein-ring "0.9.7"]]
--- src/friend_form_login/handler.clj
+++ src/friend_form_login/handler.clj
@@ -2,6 +2,11 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [clojure.data.json :as json]
+            [friend-oauth2.workflow :as oauth2]
+            [friend-oauth2.util :refer [format-config-uri]]
+            [environ.core :refer [env]]
+            [org.httpkit.client :as http]
             [cemerick.friend :as friend]
             (cemerick.friend [workflows :as workflows]
                              [credentials :as creds])))
@@ -12,6 +17,12 @@
             "dave" {:username "dave"
                     :password (creds/hash-bcrypt "password")
                     :roles #{::user}}})
+
+(def client-config
+  {:client-id (env :google-client-id)
+   :client-secret (env :google-client-secret)
+   :callback {:domain (env :google-callback-domain)
+              :path "/oauth2callback"}})
 
 (derive ::admin ::user)
 
@@ -27,8 +38,69 @@
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
   (route/not-found "Not Found"))
 
+(defn token2username [access-token & [request]]
+  "Get user's email address from the vc_user table, given their access token. 
+   If access-token is not registered in database,
+   then ask Google for the user's email address and add as new row to the vc_user table.
+   _request_ is optional: if present, it allows for more diagnostic logging than without it.
+"
+  (cond
+   (nil? access-token)
+   (do
+     (throw (Exception. (str "token2user: supplied access-token was null."))))
+
+   true
+   (do
+     (let [{:keys [status headers body error] :as resp} 
+           @(http/get 
+             (str "https://www.googleapis.com/oauth2/v1/userinfo?access_token=" access-token))]
+       (cond 
+                 
+             (not (= status 200))
+             (do
+               nil)
+                 
+             true
+             (do
+               (let [body (json/read-str body
+                                         :key-fn keyword
+                                         :value-fn (fn [k v]
+                                                     v))
+                     email (get body :email)
+                     given-name (get body :given_name)
+                     family-name (get body :family_name)
+                     picture (get body :picture)]
+                 email)))))))
+
+(defn credential-fn [token]
+  ;;lookup token in DB or whatever to fetch appropriate :roles
+;  (log/info (str "google/credential-fn token: " (:access-token token)))
+  (let [username (token2username (:access-token token))]
+    {:identity token :roles #{::user}}))
+  
+(def uri-config
+  {:authentication-uri {:url "https://accounts.google.com/o/oauth2/auth"
+                        :query {:client_id (:client-id client-config)
+                                :response_type "code"
+                                :redirect_uri (format-config-uri client-config)
+                                :scope "email"}}
+
+   :access-token-uri {:url "https://accounts.google.com/o/oauth2/token"
+                      :query {:client_id (:client-id client-config)
+                              :client_secret (:client-secret client-config)
+                              :grant_type "authorization_code"
+                              :redirect_uri (format-config-uri client-config)}}})
+
+(def google-auth-config {:client-config client-config
+                         :uri-config uri-config
+                         :login-url "/the-google-login"
+                         :credential-fn credential-fn})
+
 (def app
   (handler/site
    (friend/authenticate app-routes
    			{:credential-fn (partial creds/bcrypt-credential-fn users)
-                         :workflows [(workflows/interactive-form)]})))
+                         :workflows [(workflows/interactive-form)
+                                     (oauth2/workflow google-auth-config)
+                                     ]})))
+
