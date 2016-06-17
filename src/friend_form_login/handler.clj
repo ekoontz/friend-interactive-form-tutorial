d6523cbf69f4e8ad9b87a908aea16ac07add0c4c
Eugene Koontz <ekoontz@hiro-tan.org>
2016-06-17 14:12:01 -0700

    got local-and-google authentication working pretty well.

--- src/friend_form_login/handler.clj
+++ src/friend_form_login/handler.clj
@@ -3,6 +3,7 @@
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [clojure.data.json :as json]
+            [clojure.string :as string]
             [friend-oauth2.workflow :as oauth2]
             [clojure.tools.logging :as log]
             [friend-oauth2.util :refer [format-config-uri]]
@@ -21,16 +22,65 @@
 
 (derive ::admin ::user)
 
+(defn authentications [request]
+  "get map of client's authentications and return all of the values."
+  (vals (get-in request [:session :cemerick.friend/identity :authentications])))
+
+(defn display-authentications [request]
+  (let [auths (authentications request)]
+    (str "<h3>Your authentications:</h3><ul><li>" (string/join "</li><li>" auths) "</li></ul>")))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/authorized" request
-       (friend/authorize #{::user} "This page can only be seen by authenticated users."))
+  (GET "/" request (str "<h1>Hello World</h1>"
+                        (if (not (empty? (authentications request)))
+                          (display-authentications request))
+
+                        (if (not (empty? (authentications request)))
+                          "<p><a href='/authenticated-only'>For authenticated people only</a>")
+
+                        (if (not (empty? (authentications request)))
+                          " | <a href='/admin'>For admins only</a>")
+                        
+                        (if (empty? (authentications request))
+                          " | <a href='/login/form'>Login</a> ")
+                        
+                        (if (not (empty? (authentications request)))
+                          " | <a href='/logout'>Logout</a>")
+                        ))
+
+  (GET "/authenticated-only" request
+       (friend/authorize #{::user}
+                         (str "<h1>Welcome</h1>"
+                              (display-authentications request)
+                              "<p>This page can only be seen by authenticated users.</p>"
+                              "<a href='/'>Home</a>")))
+
   (GET "/admin" request
-       (friend/authorize #{::admin} "This page can only be seen by administrators."))
-  (GET "/login" [] (-> "login.html"
+       (friend/authorize #{::admin}
+                         (str "<h1>Welcome</h1>"
+                              (display-authentications request)
+                              "<p>This page can only be seen by administrators.</p>"
+                              "<a href='/'>Home</a>")))
+
+  ;; Note that in our configuration of friend, we've made the login-url be /login ..
+  (GET "/login" [] (-> "login.html" ;; note that this won't be used if the user is not
+                       ;; already authenticated, because we are using two workflows:
+                       ;; 1. friend-workflows/interactive-form
+                       ;; 2. oauth2/workflow
+                       ;; Friend will simply do the google workflow.
+                       ;; If however the user *is* authenticated, (whether locally or oauth2)
+                       ;; then login.html will be used and the form will be shown.
                        (ring.util.response/file-response {:root "resources"})
                        (ring.util.response/content-type "text/html")))
-  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
+
+  ;; .. but we also supply this to allow us to access the form login.
+  ;; If the user is not currently authenticated, /login will go straight to google,
+  ;; skipping our local (login.html) login page. So /login/form avoids this forwarding
+  ;; behavior that friend has, allowing us to see the local login page.
+  (GET "/login/form" [] (-> "login.html"
+                       (ring.util.response/file-response {:root "resources"})
+                       (ring.util.response/content-type "text/html")))
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/login/form")))
   (route/not-found "Not Found"))
 
 (defn token2username [access-token]
